f6fa17a2b68b68a415023ea6e41e7cdcb44155bc
Byrdann Fox <mobileupstate@gmail.com>
2014-04-26 18:47:45 -0400

    Updates...

--- js/core.js
+++ js/core.js
@@ -24,41 +24,47 @@
 		footerAnimation: function() {
 			// Animations for Mobile...
 			homeViewFooterHeight = $('#home-view-footer').height();
-			if (homeViewFooterHeight === 60) {
+			if (homeViewFooterHeight === 80) {
+				$('hgroup h1, h2').fadeOut(50);
 				$('#quest-button').fadeOut(50);
 				$('#footer-button').css('transform', 'rotate(180deg) scaleX(-1)');
 				$('#home-view-footer')
 				.stop()
 				.animate({
-					height: 195
+					height: 285
 				})
 				.end();
 				$('#footer-button')
 				.stop()
 				.animate({
-					bottom: 165
+					bottom: 255
 				})
 				.end();
 				$('#home-view-footer span, button').show();
 				$('#home-view-footer button').addClass('display-block');
 				$('#home-view-footer button').css('display', 'block');
+				$('#home-view-footer h4').hide();
+				$('#home-view-footer').css('padding-top', '2em');
 				return true; 
-			} else if (homeViewFooterHeight >= 195) {
+			} else if (homeViewFooterHeight >= 163) {
+				$('hgroup h1, h2').fadeIn(50);
 				$('#quest-button').fadeIn(500);
 				$('#footer-button').css('transform', 'rotate(360deg) scaleX(-1)');
 				$('#home-view-footer')
 				.stop()
 				.animate({
-					height: 60
+					height: 80
 				})
 				.end();
 				$('#footer-button')
 				.stop()
 				.animate({
-					bottom: 25
+					bottom: 45
 				})
 				.end();
 				$('#home-view-footer span, button').hide();
+				$('#home-view-footer h4').show();
+				$('#home-view-footer').css('padding-top', '0');
 				return true;
 			}
 			return false; 
