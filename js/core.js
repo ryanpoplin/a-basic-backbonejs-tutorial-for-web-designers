41956e0f0a5e7319103bd358b9694d9ef85468fa
Byrdann Fox <mobileupstate@gmail.com>
2014-05-07 17:16:12 -0400

    Updates...

--- js/core.js
+++ js/core.js
@@ -11,7 +11,7 @@
 			link: 'https://www.google.com/parkhop',
 			rules: 'Rules: Lorem ipsum dolor... Lorem ipsum dolor, lorem ipsum dolor. Lorem ipsum dolor, lorem ipsum dolor.',
 			prizes: 'Prizes: Lorem ipsum dolor... Lorem ipsum dolor, lorem ipsum dolor. Lorem ipsum dolor, lorem ipsum dolor.'
-		},
+		}
 	];
 
 	var app;
